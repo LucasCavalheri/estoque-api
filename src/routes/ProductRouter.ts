c044b1eb59569e0620fc1cd0ad865acf244b7464
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-08 12:39:15 -0300

    feat: adiciona rota PUT /edit para editar produto

--- src/routes/ProductRouter.ts
+++ src/routes/ProductRouter.ts
@@ -3,6 +3,7 @@ import { isAuthenticated } from '../middlewares/isAuthenticated';
 import multer from 'multer';
 import uploadConfig from '../config/multer';
 import { CreateProductController } from '../controllers/Product/CreateProductController';
+import { EditProductController } from '../controllers/Product/EditProductController';
 
 const router = Router();
 
@@ -11,5 +12,6 @@ const upload = multer(uploadConfig.upload('./tmp'));
 // http://localhost:3000/product
 
 router.post('/', isAuthenticated, upload.single('file'), new CreateProductController().handle);
+router.put('/edit', isAuthenticated, upload.single('file'), new EditProductController().handle);
 
 export default router;
