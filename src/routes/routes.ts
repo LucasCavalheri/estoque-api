49ded8cf04c052eb425eea45b91d77e716c908bd
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-08 10:40:04 -0300

    feat: adiciona configurações do multer para upload de imagens

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/multer';
 import UserRouter from './UserRouter';
 import CategoryRouter from './CategoryRouter';
 
 const router = Router();
+
+const upload = multer(uploadConfig.upload('./tmp'));
 
 router.use('/user', UserRouter);
 router.use('/category', CategoryRouter);
