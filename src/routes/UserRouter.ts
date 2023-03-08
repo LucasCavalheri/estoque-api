15efd61cb7ece87218ba78133af478a4ea216c4e
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-08 09:18:19 -0300

    feat: cria rota /remove

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthUserController } from '../controllers/User/AuthUserController';
 import { CreateUserController } from '../controllers/User/CreateUserController';
 import { DetailUserController } from '../controllers/User/DetailUserController';
+import { RemoveUserController } from '../controllers/User/RemoveUserController';
 import { isAuthenticated } from '../middlewares/isAuthenticated';
 
 const router = Router();
@@ -11,5 +12,6 @@ const router = Router();
 router.get('/me', isAuthenticated, new DetailUserController().handle);
 router.post('/', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle);
+router.delete('/remove', new RemoveUserController().handle);
 
 export default router;
