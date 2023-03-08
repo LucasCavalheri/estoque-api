824c95891e9d190a07be855f0a8a8645baf6a0cd
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-08 09:04:17 -0300

    feat: cria rota /me

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import { AuthUserController } from '../controllers/User/AuthUserController';
 import { CreateUserController } from '../controllers/User/CreateUserController';
+import { DetailUserController } from '../controllers/User/DetailUserController';
+import { isAuthenticated } from '../middlewares/isAuthenticated';
 
 const router = Router();
 
 // http://localhost:3000/user
 
+router.get('/me', isAuthenticated, new DetailUserController().handle);
 router.post('/', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle);
 
