651e9b6eb4a56b27bfa71dc7104c8657eaec2e40
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-08 10:16:21 -0300

    feat: adiciona rota GET /all para listar as categorias

--- src/routes/CategoryRouter.ts
+++ src/routes/CategoryRouter.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { CreateCategoryController } from '../controllers/Category/CreateCategoryController';
 import { EditCategoryController } from '../controllers/Category/EditCategoryController';
+import { ListCategoryController } from '../controllers/Category/ListCategoryController';
 import { isAuthenticated } from '../middlewares/isAuthenticated';
 
 const router = Router();
 
 // http://localhost:3000/category
 
+router.get('/all', isAuthenticated, new ListCategoryController().handle);
 router.post('/', isAuthenticated, new CreateCategoryController().handle);
 router.put('/edit', isAuthenticated, new EditCategoryController().handle);
 
