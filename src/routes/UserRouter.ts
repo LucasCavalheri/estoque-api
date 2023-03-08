81e0e763b8bee0c041d5ab0832387f060f1f7c7a
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-08 08:14:30 -0300

    feat: cria rota /session

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import { AuthUserController } from '../controllers/User/AuthUserController';
 import { CreateUserController } from '../controllers/User/CreateUserController';
 
 const router = Router();
 
+// http://localhost:3000/user
+
 router.post('/', new CreateUserController().handle)
+router.post('/session', new AuthUserController().handle);
 
 export default router;
