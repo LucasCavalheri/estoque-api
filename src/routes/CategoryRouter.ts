fdb546b474bcfd901cb3f9ef9c326226522652c8
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-08 09:56:31 -0300

    feat: adiciona rota PUT /edit para editar categoria

--- src/routes/CategoryRouter.ts
+++ src/routes/CategoryRouter.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { CreateCategoryController } from '../controllers/Category/CreateCategoryController';
+import { EditCategoryController } from '../controllers/Category/EditCategoryController';
 import { isAuthenticated } from '../middlewares/isAuthenticated';
 
 const router = Router();
@@ -7,5 +8,6 @@ const router = Router();
 // http://localhost:3000/category
 
 router.post('/', isAuthenticated, new CreateCategoryController().handle);
+router.put('/edit', isAuthenticated, new EditCategoryController().handle);
 
 export default router;
