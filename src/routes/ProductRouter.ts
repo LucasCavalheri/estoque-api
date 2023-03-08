a93357b1b3492ef115eb07846681bd2e17360ee0
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-08 13:06:38 -0300

    feat: adiciona rota GET / para buscar produtos pelo id de uma categoria

--- src/routes/ProductRouter.ts
+++ src/routes/ProductRouter.ts
@@ -4,6 +4,7 @@ import multer from 'multer';
 import uploadConfig from '../config/multer';
 import { CreateProductController } from '../controllers/Product/CreateProductController';
 import { EditProductController } from '../controllers/Product/EditProductController';
+import { ListProductByCategoryIdController } from '../controllers/Product/ListProductByCategoryIdController';
 
 const router = Router();
 
@@ -11,6 +12,7 @@ const upload = multer(uploadConfig.upload('./tmp'));
 
 // http://localhost:3000/product
 
+router.get('/', isAuthenticated, new ListProductByCategoryIdController().handle);
 router.post('/', isAuthenticated, upload.single('file'), new CreateProductController().handle);
 router.put('/edit', isAuthenticated, upload.single('file'), new EditProductController().handle);
 
