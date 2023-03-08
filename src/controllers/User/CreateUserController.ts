18756106d42ca135a94972c7375b93d406d98da5
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-07 21:00:51 -0300

    feat: adiciona função para criar usuário no CreateUserController

---
+++ src/controllers/User/CreateUserController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { CreateUserService } from '../../services/User/CreateUserService';
+import { UserRequest } from '../../models/User/interfaces/UserRequest';
+
+class CreateUserController {
+  async handle(req: Request, res: Response) {
+    const { name, email, password }: UserRequest = req.body;
+    const createUserService = new CreateUserService();
+
+    const user = await createUserService.execute({ name, email, password });
+
+    return res.status(201).json(user);
+  }
+}
+
+export { CreateUserController };
