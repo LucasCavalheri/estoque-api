5d71008cb6983e8587c5de7fa5669f5decc8c030
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-07 21:00:38 -0300

    feat: adiciona função para criar usuário no CreateUserService

---
+++ src/services/User/CreateUserService.ts
@@ -0,0 +1,36 @@
+import prismaClient from '../../prisma/index';
+import { hash } from 'bcryptjs';
+import { UserRequest } from '../../models/User/interfaces/UserRequest';
+
+class CreateUserService {
+  async execute({ name, email, password }: UserRequest) {
+    if (!email) throw new Error('Email is not provided');
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (userAlreadyExists) throw new Error('Email already exists');
+
+    const passwordHash = await hash(password, 8);
+
+    const user = await prismaClient.user.create({
+      data: {
+        name,
+        email,
+        password: passwordHash,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
+  }
+}
+
+export { CreateUserService };
