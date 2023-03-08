2b398be88e29469e153b41eb622b03622a129ca2
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-08 09:30:03 -0300

    refactor: adiciona const PORT

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import app from './app';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const { PORT } = process.env || 3333;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
