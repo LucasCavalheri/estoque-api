f359c780ccab613be2a8fe5cd1ff327e467002db
LucasCavalheri <cav.catecumenal@gmail.com>
2023-03-08 10:30:05 -0300

    feat: adiciona rota DELETE /remove para remover uma categoria

--- src/routes/CategoryRouter.ts
+++ src/routes/CategoryRouter.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { CreateCategoryController } from '../controllers/Category/CreateCategoryController';
 import { EditCategoryController } from '../controllers/Category/EditCategoryController';
 import { ListCategoryController } from '../controllers/Category/ListCategoryController';
+import { RemoveCategoryController } from '../controllers/Category/RemoveCategoryController';
 import { isAuthenticated } from '../middlewares/isAuthenticated';
 
 const router = Router();
@@ -11,5 +12,6 @@ const router = Router();
 router.get('/all', isAuthenticated, new ListCategoryController().handle);
 router.post('/', isAuthenticated, new CreateCategoryController().handle);
 router.put('/edit', isAuthenticated, new EditCategoryController().handle);
+router.delete('/remove', isAuthenticated, new RemoveCategoryController().handle);
 
 export default router;
